41557d31d188b25e6743d1fe16fcdfd76dfa1d3f
krsdata <kroy.iips@gmail.com>
2020-03-23 23:20:44 +0530

    leaderboard

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('about-us', function () {
+      echo "about-us";
+});
+
+
+Route::get('privacy-policy', function () {
+      echo "privacy-policy";
+});
+
+
+Route::get('terms-and-conditions', function () {
+      echo "terms-and-conditions";
+});
+
+
+Route::get('legality', function () {
+      echo "legality";
+});
+
+
+Route::get('how-to-play', function () {
+      echo "how-to-play";
+});
+
+Route::get('fantasy-points-system', function () {
+      echo "fantasy-points-system";
+});
