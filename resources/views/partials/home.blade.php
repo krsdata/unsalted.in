3be493b5f5a82df3b6b323d4ae06164b42abe2f7
krsdata <kroy.iips@gmail.com>
2020-04-12 13:30:29 +0530

    logo

--- resources/views/partials/home.blade.php
+++ resources/views/partials/home.blade.php
@@ -1,8 +1,6 @@
-      
-
-     
-
-      <section class="hero-wrap js-fullheight" style="background-image: url('{{ URL::asset("webmedia/images/bg_2.jpg") }}'); " data-section="home" data-stellar-background-ratio="0.5" id="home-section">
+     <?php $bgUrl = URL::asset("webmedia/images/bg_2.jpg"); ?>
+      <section class="hero-wrap js-fullheight" style="background-image: url('<?php echo $bgUrl; ?>')" data-section="home" data-stellar-background-ratio="0.5" id="home-section">
+        <img src="">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-start" data-scrollax-parent="true">
